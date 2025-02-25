0c1d1d0df556c83e8e92d0b9502de7c55f2e709c
Sai Snehitha Ravuru <61641905+SaiSnehithaRavuru@users.noreply.github.com>
2025-02-25 10:38:56 +0530

    Re-commiting the final code

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="center">
+                All rights reserved &copy; Sai Snehitha Ravuru
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
